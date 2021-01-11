8863dfe947aec545b02259255f9da5f4a3714e13
Prince Codes 247 <princecodes247@gmail.com>
2021-01-11 07:17:18 -0800

    feat: basic setup

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res, next) => {
+    res.send('<p>HI</p>')
+})
+
+app.listen(5000, port => console.log(`Listening on port ${port}`))

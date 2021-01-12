0961256e7b46e1febbb4e58cafbfbf14151451ae
Prince Codes 247 <princecodes247@gmail.com>
2021-01-12 14:21:31 -0800

    ..

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
-import "dotenv/config";
+
 const app = express()
-const port = process.env.PORT;
+
 
 app.get('/', (req, res, next) => {
     res.send('<p>HI</p>')
 })
 
-app.listen(port, port => console.log(`Listening on port ${port}`))
+app.listen(5000, port => console.log(`Listening on port ${port}`))
